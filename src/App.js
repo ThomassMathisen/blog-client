f3b5eb8bd9e97b843e57d1e9430ddf59d06d3112
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-06 14:33:19 +0100

    Create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TopBar from "./components/topbar/TopBar";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <>
       <TopBar/>
+      <Homepage />
     </>
   )
 }
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/Header"
+import "./homepage.css"
+
+export default function Homepage() {
+  return (
+    <div className="home">
+      <Header />
+      Homepage
+    </div>
+  )
+}
