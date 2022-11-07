a14b1917fec88a68a5ab4dccfbd6b1ee8df1b08f
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-07 16:29:44 +0100

    Setup page for writing blog posts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import TopBar from "./components/topbar/TopBar";
-import Homepage from "./pages/homepage/Homepage";
-import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <TopBar/>
-      <Single />
+      <Write />
     </>
   )
 }
